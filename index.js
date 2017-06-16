6462a82ae1d47599d0860afc52b46a97ba877049
caleblj <joh16002@byui.edu>
2017-06-16 12:23:04 -0600

    prove 9

---
+++ index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use('/', express.static('static'));
+
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+function calcPostageLettersMetered(wieght){
+	if (weight == 1){
+		return 0.46;
+	}
+}
+
+add.post('/getData', funciton(request, response){
+
+	//
+	var result,
+	weight = Number(request.body.weight);
+
+	switch(Number(t.body.type){
+		case 2 :
+		result = calcPostageLettersMetered(weight);
+		break
+	}
+
+	response.render('result', {
+		result: request.body.wieght
+
+	})
+console.log(request.body.wight);
+console.log(request.body.type);
+});
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('server is listening');
+})
